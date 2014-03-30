d560687751588d8a5d2ed24262d7605f07e7df22
Bartek Malecki <bartekm93+gitlab@gmail.com>
2014-03-30 19:37:47 +0200

    backup

--- test_thread_guard.hpp
+++ test_thread_guard.hpp
@@ -2,7 +2,8 @@
 #define TEST_THREAD_GUARD_HPP
 
 #include <thread>
-#include "boost/utility.hpp"
+#include <stdexcept>
+#include "boost/noncopyable.hpp"
 
 class Thread_guard
  : boost::noncopyable
@@ -19,6 +20,25 @@ class Thread_guard
       }
    private:
       std::thread& t_;
+};
+
+class Scoped_thread
+ : boost::noncopyable
+{
+   public:
+      explicit Scoped_thread(std::thread t)
+         : t_(std::move(t))
+      {
+         if(!t_.joinable())
+            throw std::logic_error("Brak watku");
+      }
+
+      ~Scoped_thread()
+      {
+         t_.join();
+      }
+   private:
+      std::thread t_;
 };
 
 #endif
