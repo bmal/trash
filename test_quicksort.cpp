47e5e9814e021dda4f22878e1fa39783fb0a7ffd
Bartłomiej Małecki <bartekm93+gitlab@gmail.com>
2014-04-06 12:12:51 +0200

    quicksort

---
+++ test_quicksort.cpp
@@ -0,0 +1,88 @@
+#include <chrono>
+#include <limits>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+list<T> sequential_quicksort(list<T> input)
+{
+   if(input.empty())
+      return input;
+
+   list<T> result;
+   result.splice(result.begin(), input, input.begin());
+   const T& privot = *result.begin();
+
+   auto divide_point = partition(input.begin(), input.end(), 
+         [&](T const& t){return t < privot;});
+
+   list<T> lower_part;
+   lower_part.splice(lower_part.end(), input, input.begin(), divide_point);
+
+   auto new_lower(sequential_quicksort(move(lower_part)));
+   auto new_higher(sequential_quicksort(move(input)));
+
+   result.splice(result.end(), new_higher);
+   result.splice(result.begin(), new_lower);
+   return result;
+};
+
+template<typename T>
+list<T> parallel_quicksort(list<T> input)
+{
+   if(input.empty())
+      return input;
+
+   list<T> result;
+   result.splice(result.begin(), input, input.begin());
+   const T& privot = *result.begin();
+
+   auto divide_point = partition(input.begin(), input.end(), 
+         [&](T const& t){return t < privot;});
+
+   list<T> lower_part;
+   lower_part.splice(lower_part.end(), input, input.begin(), divide_point);
+
+   future<list<T> > new_lower(async(parallel_quicksort<T>, move(lower_part)));
+   auto new_higher(parallel_quicksort(move(input)));
+
+   result.splice(result.end(), new_higher);
+   result.splice(result.begin(), new_lower.get());
+   return result;
+};
+
+int main()
+{
+   vector<int> v;
+   for(int i = 0; i < 100000; ++i)
+      v.push_back(i);
+   random_shuffle(v.begin(), v.end());
+   list<int> l;
+   for(vector<int>::size_type i = 0; i < v.size(); ++i)
+      l.push_back(v[i]);
+   auto s_start = chrono::steady_clock::now();
+   l = sequential_quicksort(l);
+   auto s_end = chrono::steady_clock::now();
+   cout << "Calkowity czas: " << chrono::duration_cast<chrono::milliseconds>(s_end - s_start).count() << endl;
+
+   l.clear();
+   for(vector<int>::size_type i = 0; i < v.size(); ++i)
+      l.push_back(v[i]);
+   auto p_start = chrono::steady_clock::now();
+   l = sequential_quicksort(l);
+   auto p_end = chrono::steady_clock::now();
+   cout << "Calkowity czas: " << chrono::duration_cast<chrono::milliseconds>(p_end - p_start).count() << endl;
+
+   l.clear();
+   for(vector<int>::size_type i = 0; i < v.size(); ++i)
+      l.push_back(v[i]);
+   auto start = chrono::steady_clock::now();
+   l.sort();
+   auto end = chrono::steady_clock::now();
+   cout << "Calkowity czas: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
+}
